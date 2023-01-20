5712bd07467e43028c3e792a98e0d309f014af5c
Gustavo Targino <gustavo.targino03@hotmail.com>
2023-01-20 01:11:11 -0300

    FEAT / Pagina de cuidados em alagamentos

--- user/home.php
+++ user/home.php
@@ -3,6 +3,9 @@
 <h3 class="my-5">O que deseja fazer?</h3>
 
 <a href="?pg=registrar" class="btn">Registrar alagemento</a>
+
+
+<a href="?pg=prevencao" class="btn">Cuidados em alagamentos</a>
     
 
 
--- user/topo.php
+++ user/topo.php
@@ -1,29 +1,3 @@
-<?php
-    include("./inc/config.inc.php");
-?>
-
-<!-- <header>
-        <nav class="navbar navbar-expand-sm align-items-baseline">
-            <a href="?pg=principal" class="navbar-brand">
-                <img src="./assets/Logo_User.png" alt="logo_alag" id="logo"> 
-                <strong>- ALAG <i class="fa-solid fa-triangle-exclamation"></i></strong> 
-            </a>
-
-            <div class="navbar-nav">
-                <a href="?pg=principal" class="nav-item nav-link" id="home-menu"><strong>Home</strong></a>
-                <a href="?pg=faleconosco" class="nav-item nav-link" id="talk-menu"><strong>Denúncia</strong></a>
-                <a href="?pg=faleconosco" class="nav-item nav-link" id="talk-menu"><strong>Previsão</strong></a>
-                <a href="?pg=faleconosco" class="nav-item nav-link" id="talk-menu"><strong>Sobre</strong></a>
-        </nav>
-    </div>
-</header> -->
-
-<!-- <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    
-  </div>
-</nav> -->
-
 <nav class="navbar navbar-dark">
   <div class="container-fluid">
   <a class="navbar-brand" href="?pg=home">
@@ -50,6 +24,10 @@
 
           <li class="nav-item">
             <a class="nav-link" href="#">Previsão do tempo</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="?pg=prevencao">Cuidados em alagamentos</a>
           </li>
           
       </div>
